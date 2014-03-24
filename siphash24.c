085171ce7fb8954c8023cb59e1b494f0fd13a4b5
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2014-03-24 15:47:11 +0100

    copyright

--- siphash24.c
+++ siphash24.c
@@ -1,9 +1,8 @@
 /*
    SipHash reference C implementation
 
-   Written in 2012 by
-   Jean-Philippe Aumasson <jeanphilippe.aumasson@gmail.com>
-   Daniel J. Bernstein <djb@cr.yp.to>
+   Copyright (c) 2012 Jean-Philippe Aumasson <jeanphilippe.aumasson@gmail.com>
+   Copyright (c) 2012 Daniel J. Bernstein <djb@cr.yp.to>
 
    To the extent possible under law, the author(s) have dedicated all copyright
    and related and neighboring rights to this software to the public domain
